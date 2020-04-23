7070b948fe36a1c096958fee8a5e50e3f9456198
Krzysztof <krzys9990@gmail.com>
2020-04-23 08:48:57 +0200

    Lab 3

---
+++ lab3/scripts/kdl.py
@@ -0,0 +1,129 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import json
+import rospy
+import rospkg
+import PyKDL
+from sensor_msgs.msg import JointState
+from tf.transformations import *
+from geometry_msgs.msg import PoseStamped
+
+def get_parameters(param_name):
+    if rospy.has_param(param_name):
+        ros_param_name='/'+param_name
+        param_value=rospy.get_param(ros_param_name)
+        param_value_string=str(param_value)
+        
+        if param_value>=0 and isinstance(param_value, (int, float)):
+            #print("Succesful initialisation of parameter: " + ros_param_name)
+            return True
+        else:
+            
+            #print("Unsuccesful initialisation of parameter: " + ros_param_name)
+            #print("Length value must be a positive number")
+            return False
+    else:
+        #print("There is no parameter called /" + param_name + ". Proceeding with default length value.")
+        return False
+
+
+def callback(data):
+    kdl_chain =PyKDL.Chain()   
+    Frame = PyKDL.Frame();
+
+    i = 0
+    d=0
+    th=0
+    for joint in dh_file:
+        name = joint['name']
+        last_d = d
+        last_th = th
+        a = joint["a"]
+        d = joint["d"]
+        al = joint["al"]
+        th = joint["th"]
+
+        if name == 'i3' and get_parameters('dlugosc1'):
+        	a = rospy.get_param("/dlugosc1")
+
+        if name == 'hand' and get_parameters('dlugosc2'):
+            a = rospy.get_param("/dlugosc2")
+
+        #forego first iteration
+        if i==0:
+            i = i+1
+            continue
+        
+        kdl_chain.addSegment(PyKDL.Segment(PyKDL.Joint(PyKDL.Joint.RotZ), Frame.DH(a, al, last_d, last_th)))
+        
+        i = i+1
+    
+    jointDisplacement = PyKDL.JntArray(kdl_chain.getNrOfJoints())
+    
+    #joint displacements including restrictions
+    jointDisplacement[0] = data.position[0]
+    jointDisplacement[1] = data.position[1]
+    jointDisplacement[2] = data.position[2]
+
+    if(data.position[0] < restrictions[0]['backward']):
+        jointDisplacement[0] = restrictions[0]['backward']
+        rospy.logerr("[KDL] Przekroczono ograniczenie dolne stawu o numerze: 1")
+    elif(data.position[0] > restrictions[0]['forward']):
+        jointDisplacement[0] = restrictions[0]['forward']
+        rospy.logerr("[KDL] Przekroczono ograniczenie gorne stawu o numerze: 1")
+
+    if(data.position[1] < restrictions[1]['backward']):
+        jointDisplacement[1] = restrictions[1]['backward']
+        rospy.logerr("[KDL] Przekroczono ograniczenie dolne stawu o numerze: 2")
+    elif(data.position[1] > restrictions[1]['forward']):
+        jointDisplacement[1] = restrictions[1]['forward']
+        rospy.logerr("[KDL] Przekroczono ograniczenie gorne stawu o numerze: 2")
+
+    if(data.position[2] < restrictions[2]['backward']):
+        jointDisplacement[2] = restrictions[2]['backward']
+        rospy.logerr("[KDL] Przekroczono ograniczenie dolne stawu o numerze: 3")
+    elif(data.position[2] > restrictions[2]['forward']):
+        jointDisplacement[2] = restrictions[2]['forward']
+        rospy.logerr("[KDL] Przekroczono ograniczenie gorne stawu o numerze: 3")
+
+    f_k_solver = PyKDL.ChainFkSolverPos_recursive(kdl_chain)
+
+    frame = PyKDL.Frame()
+    f_k_solver.JntToCart(jointDisplacement, frame)
+    quatr = frame.M.GetQuaternion()
+    
+    kdl_pose = PoseStamped()
+    kdl_pose.header.frame_id = 'base_link'
+    kdl_pose.header.stamp = rospy.Time.now()
+
+    kdl_pose.pose.position.x = frame.p[0]
+    kdl_pose.pose.position.y = frame.p[1]
+    kdl_pose.pose.position.z = frame.p[2]
+    kdl_pose.pose.orientation.x = quatr[0]
+    kdl_pose.pose.orientation.y = quatr[1]
+    kdl_pose.pose.orientation.z = quatr[2]
+    kdl_pose.pose.orientation.w = quatr[3]
+    
+    pub.publish(kdl_pose)
+    #print(data)
+
+
+if __name__ == '__main__':
+
+    rospack = rospkg.RosPack()
+    
+    rospy.init_node('KDL_DKIN', anonymous=False)
+    dh_file ={}
+    restrictions = {}
+
+    with open(rospack.get_path('lab3') + '/dh_parameters.json', 'r') as file:
+        dh_file= json.loads(file.read())
+
+    with open(rospack.get_path('lab3') + '/restrictions.json', 'r') as file:
+        restrictions = json.loads(file.read())
+
+    pub = rospy.Publisher('KdlAxes', PoseStamped, queue_size=10)
+    rospy.Subscriber("joint_states", JointState , callback)
+    
+    rospy.spin()
---
+++ lab3/scripts/nonkdl.py
@@ -0,0 +1,113 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import json
+import rospy
+import rospkg
+from sensor_msgs.msg import JointState
+from tf.transformations import *
+from geometry_msgs.msg import PoseStamped
+
+def get_parameters(param_name):
+    if rospy.has_param(param_name):
+        ros_param_name='/'+param_name
+        param_value=rospy.get_param(ros_param_name)
+        param_value_string=str(param_value)
+        
+        if param_value>=0 and isinstance(param_value, (int, float)):
+            #print("Succesful initialisation of parameter: " + ros_param_name)
+            return True
+        else:
+            
+            #print("Unsuccesful initialisation of parameter: " + ros_param_name)
+            #print("Length value must be a positive number")
+            return False
+    else:
+        #print("There is no parameter called /" + param_name + ". Proceeding with default length value.")
+        return False
+
+
+
+def callback(data):
+    
+    main_matrix = translation_matrix((0, 0, 0));
+    
+    
+    i=0
+    for joint in dh_file:
+        name = joint['name']
+        a = joint["a"]
+        d = joint["d"]
+        al = joint["al"]
+        th=joint['th']
+        
+
+        if name == 'i3' and get_parameters('dlugosc1'):
+        	a = rospy.get_param("/dlugosc1")
+
+        if name == 'hand' and get_parameters('dlugosc2'):
+            a = rospy.get_param("/dlugosc2")
+
+        matrixD = translation_matrix((0, 0, d))
+        if name != 'hand':
+            pos=data.position[i]
+            if i>=0:
+                if restrictions[i]['backward']>pos:
+                    pos = restrictions[i]['backward']
+                    warn_string="[NONKDL] Przekroczono ograniczenie dolne stawu o numerze: "+str(i+1)
+                    rospy.logerr(warn_string)
+                elif restrictions[i]['forward']<pos:
+                    pos = restrictions[i]['forward']
+                    warn_string="[NONKDL] Przekroczono ograniczenie gorne stawu o numerze: "+str(i+1)
+                    rospy.logerr(warn_string)
+            matrixTheta = rotation_matrix(th+pos, z_axis)
+        else:
+            matrixTheta = rotation_matrix(th, z_axis)
+        matrixA = translation_matrix((a, 0, 0))
+        matrixAlpha = rotation_matrix(al, x_axis)
+
+        trans_matrix = concatenate_matrices(matrixA,matrixAlpha,matrixTheta, matrixD)
+        main_matrix = concatenate_matrices(main_matrix, trans_matrix)
+        i = i + 1
+        
+    
+    x, y, z = translation_from_matrix(main_matrix)
+    qx, qy, qz, qw = quaternion_from_matrix(main_matrix)
+    non_kdl_pose = PoseStamped()
+    non_kdl_pose.header.frame_id = 'base_link'
+    non_kdl_pose.header.stamp = rospy.Time.now()
+    
+    non_kdl_pose.pose.position.x = x
+    non_kdl_pose.pose.position.y = y
+    non_kdl_pose.pose.position.z = z
+    non_kdl_pose.pose.orientation.x = qx
+    non_kdl_pose.pose.orientation.y = qy
+    non_kdl_pose.pose.orientation.z = qz
+    non_kdl_pose.pose.orientation.w = qw
+
+    pub.publish(non_kdl_pose)
+    #print(data)
+
+if __name__ == '__main__':
+    
+    x_axis = (1,0,0)
+    y_axis = (0,1,0)
+    z_axis = (0,0,1)
+
+    rospack = rospkg.RosPack()
+    
+    rospy.init_node('NONKDL_DKIN', anonymous=False)
+    dh_file ={}
+    restr_file={}
+    
+    with open(rospack.get_path('lab3') + '/dh_parameters.json', 'r') as file:
+        dh_file= json.loads(file.read())
+    
+    with open(rospack.get_path('lab3') + '/restrictions.json', 'r') as file:
+        restrictions= json.loads(file.read())
+    
+    
+    pub = rospy.Publisher('NoKdlAxes', PoseStamped, queue_size=10)
+    rospy.Subscriber("joint_states", JointState , callback)
+    
+    rospy.spin()
